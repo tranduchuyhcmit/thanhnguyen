146002c424f981275626e5928306c152cd542a61
Trần Đức Huy <tranduchuyhcm.it@gmail.com>
2019-09-23 14:30:26 +0700

    Fix Error Init()

---
+++ ThanhNguyen.Data/Infrastructure/Disposable.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThanhNguyen.Data.Infrastructure
+{
+    public class Disposable : IDisposable
+    {
+        private bool isDisposed;
+
+        ~Disposable()
+        {
+            Dispose(false);
+        }
+
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (!isDisposed && disposing)
+            {
+                Dispose();
+            }
+
+            isDisposed = true;
+        }
+
+        //Override this to dispose custom object (Ghi đè lên điều này để loại bỏ đối tượng tùy chỉnh)
+        protected virtual void DisposeCore()
+        { 
+        }
+    }
+}
---
+++ ThanhNguyen.Data/ThanhNguyenDbContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThanhNguyen.Model.Models;
+
+namespace ThanhNguyen.Data
+{
+    class ThanhNguyenDbContext : DbContext
+    {
+        public ThanhNguyenDbContext() : base("ThanhNguyenConnection")
+        {
+            //Cấu hình cho Lazy Loading
+            //Khi load 1 cái bảng ra không tự động include thêm bảng con nữa
+            this.Configuration.LazyLoadingEnabled = false;
+        }
+
+        //Khai báo danh sách các bảng
+        public DbSet<Footer> Footers { set; get; }
+        public DbSet<Menu> Menus { set; get; }
+        public DbSet<MenuGroup> MenuGroups { set; get; }
+        public DbSet<Order> Orders { set; get; }
+        public DbSet<OrderDetail> OrderDetails { set; get; }
+        public DbSet<Page> Pages { set; get; }
+        public DbSet<Post> Posts { set; get; }
+        public DbSet<PostCategory> PostCategories { set; get; }
+        public DbSet<PostTag> PostTags { set; get; }
+        public DbSet<Product> Products { set; get; }
+        public DbSet<ProductCategory> ProductCategories { set; get; }
+        public DbSet<ProductTag> ProductTags { set; get; }
+        public DbSet<Slide> Slides { set; get; }
+        public DbSet<SupportOnline> SupportOnlines { set; get; }
+        public DbSet<SystemConfig> SystemConfigs { set; get; }
+        public DbSet<Tag> Tags { set; get; }
+        public DbSet<VisitorStatistic> VisitorStatistics { set; get; }
+
+        public static ThanhNguyenDbContext Context()
+        {
+            return new ThanhNguyenDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder builder)
+        {
+            
+        }
+    }
+}
